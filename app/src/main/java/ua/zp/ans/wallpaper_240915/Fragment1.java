259a0be45592aa9e98c7e1ca300fdc8b61f8d669
DanielAndreiev <daniel.new.world@gmail.com>
2023-09-05 21:19:28 +0300

    init

---
+++ app/src/main/java/ua/zp/ans/wallpaper_240915/Fragment1.java
@@ -0,0 +1,20 @@
+package ua.zp.ans.wallpaper_240915;
+
+/**
+ * Created by root on 24.09.15.
+ */
+
+        import android.app.Fragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, null);
+    }
+}
